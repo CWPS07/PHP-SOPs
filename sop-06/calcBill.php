aa5adbf93f7404a90c9aff31c7ac5432a240269f
CWPS07 <doreps07@gmail.com>
2026-05-15 23:08:55 +0530

    Update calcBill.php code

--- sop-06/calcBill.php
+++ sop-06/calcBill.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+$units = 250;
+$bill = 0;
+
+if($units <= 100)
+{
+    $bill = $units * 4;
+}
+else if($units <= 200)
+{
+    $bill = (100 * 4) + (($units - 100) * 5);
+}
+else
+{
+    $bill = (100 * 4) + (100 * 5) + (($units - 200) * 6);
+}
+
+echo "Total Bill = Rs. " . $bill;
+?>
+
+</body>
+</html>
