925a6e0e51698d8e75d35cc253c477b5e585dde1
CWPS07 <doreps07@gmail.com>
2026-05-15 23:11:25 +0530

    Update studentInfo.php code

--- sop-07/studentInfo.php
+++ sop-07/studentInfo.php
@@ -1,1 +1,41 @@
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post">
+    Roll No: <input type="text" name="rollno" required><br><br>
+    Name: <input type="text" name="name" required><br><br>
+    <input type="submit" name="save" value="Save">
+</form>
+
+<?php
+$host = "localhost";
+$user = "root";      
+$pass = "";          
+$db   = "test";      
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+if (isset($_POST['save'])) {
+    $roll = $_POST['rollno'];
+    $name = $_POST['name'];
+
+    $query = "INSERT INTO student (rollno, name) VALUES ('$roll', '$name')";
+
+    if (mysqli_query($conn, $query)) {
+        echo "Record Inserted Successfully";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+</body>
+</html>
