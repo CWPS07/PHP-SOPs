84c25152f0b72b46cee1803199f6c1b8e9e5ee54
CWPS07 <doreps07@gmail.com>
2026-05-15 23:07:10 +0530

    Update marks.php code

--- sop-04/marks.php
+++ sop-04/marks.php
@@ -1,1 +1,30 @@
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$marks = array(
+    "English" => 81,
+    "Hindi" => 80,
+    "Marathi" => 86,
+    "Maths" => 80,
+    "IT" => 90
+);
+
+$total = array_sum($marks);
+$percentage = $total / count($marks);
+
+echo "<h3>Subject Marks</h3>";
+
+foreach($marks as $subject => $mark)
+{
+    echo $subject . " : " . $mark . "<br>";
+}
+
+echo "<br>Total Marks = " . $total;
+echo "<br>Percentage = " . $percentage . "%";
+?>
+
+</body>
+</html>
