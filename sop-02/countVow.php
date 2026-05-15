6583f9242f62b63a62b11217890ff91ff6823c38
CWPS07 <doreps07@gmail.com>
2026-05-15 23:05:47 +0530

    Update countVow.php

--- sop-02/countVow.php
+++ sop-02/countVow.php
@@ -1,1 +1,37 @@
 
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post">
+    Enter String:
+    <input type="text" name="str">
+    <input type="submit" value="Count Vowels">
+</form>
+
+<?php
+function countVowels($str)
+{
+    $count = 0;
+    $str = strtolower($str);
+
+    for($i = 0; $i < strlen($str); $i++)
+    {
+        if($str[$i]=='a' || $str[$i]=='e' || $str[$i]=='i' || $str[$i]=='o' || $str[$i]=='u' || $str[$i]=='A' || $str[$i]=='E' || $str[$i]=='I' || $str[$i]=='O' || $str[$i]=='U')
+        {
+            $count++;
+        }
+    }
+
+    return $count;
+}
+
+if($_POST)
+{
+    $string = $_POST['str'];
+    echo "Total vowels are: " . countVowels($string);
+}
+?>
+
+</body>
+</html>
