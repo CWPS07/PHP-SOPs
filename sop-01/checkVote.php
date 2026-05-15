99ec0f1ba4cdae8fce549228f8c50ec498c86e95
CWPS07 <doreps07@gmail.com>
2026-05-15 22:58:12 +0530

    Update checkVote.php

--- sop-01/checkVote.php
+++ sop-01/checkVote.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+function checkVote($age)
+{
+    if($age >= 18)
+    {
+        echo "The person is eligible to vote.";
+    }
+    else
+    {
+        echo "The person is not eligible to vote. Minimum age required is 18.";
+    }
+}
+
+$age = 20;
+checkVote($age);
+?>
+
+</body>
+</html>
