699e4a997a02926f78cb47a06e9b4c7f4e970be6
CWPS07 <doreps07@gmail.com>
2026-05-15 23:06:31 +0530

    Update arrayOp.php code

--- sop-03/arrayOp.php
+++ sop-03/arrayOp.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+$student = array(
+    "RollNo" => 101,
+    "Name" => "Dhruv",
+    "City" => "Pune"
+);
+
+echo "<h3>Array Elements with Keys</h3>";
+
+foreach($student as $key => $value)
+{
+    echo $key . " : " . $value . "<br>";
+}
+
+echo "<h3>Size of Array</h3>";
+echo count($student);
+
+echo "<h3>Delete Element</h3>";
+
+unset($student["City"]);
+
+foreach($student as $key => $value)
+{
+    echo $key . " : " . $value . "<br>";
+}
+?>
+
+</body>
+</html>
